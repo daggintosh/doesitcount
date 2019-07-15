4d7bc0fbb00667d47478f2be0f248f00d63609bf
Dagg <theamazingdoge91@gmail.com>
2019-07-14 20:38:45 -0700

    Finished "project"

---
+++ lucid.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <time.h>
+#include <algorithm>
+
+using namespace std;
+
+string fact [10] = {"foxes aren't purple",
+"foxes have noses", 
+"foxes sometimes scream", 
+"fox bite", 
+"h", 
+"foxes do not know how to exist", 
+"fox mccloud won my smash bros tourny",
+"some foxes arent red",
+"im going to get banned",
+"lucidi is a human"};
+
+int main() {
+    srand (time(0));
+    random_shuffle(fact[0], fact[10]);
+    int factnum = rand() % 10;
+    cout << "Did you know..." << endl << fact[factnum] << endl;
+    return 0;
+}
